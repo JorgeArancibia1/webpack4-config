7e74f42c1c5cac409b1c9f5a1c095ff45897d1ef
koke <jorge.arancibia.jeal@gmail.com>
2019-09-21 02:03:47 -0300

    Se saco style-loader para dejar de inyectar los css en los archivos js y se agregaron los plugins de htmlWebpackPlugin y miniCSSExtractPlugin para tener un css aparte

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'index.js'),
-    mode: 'development', 
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules:[
-        {
-          test:/\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        }
-      ]
-    }
+  entry: path.resolve(__dirname, 'index.js'),
+  mode: 'development', 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules:[
+      {
+        test:/\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title: 'Plugins'
+    }),
+    new MiniCssExtractPlugin({
+      filename:'css/[name].css'
+    })
+  ]
 }
