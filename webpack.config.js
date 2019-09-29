f1a9532ed928a5e44a436e40e29bd5eb0f47b13e
koke <jorge.arancibia.jeal@gmail.com>
2019-09-28 23:36:47 -0300

    webpack de desarrollo listo

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'index.js'),
   mode: 'development', 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
@@ -27,7 +27,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options:{
-              limit: 90000
+              limit: 1000
             }
           },
         ]
@@ -35,9 +35,9 @@ module.exports = {
     ]
   },
   plugins:[
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Webpack 4'
+      title: 'Webpack 4',
+      template: path.resolve(__dirname, 'public/index.html')
     }),
     new MiniCssExtractPlugin({
       filename:'css/[name].css'
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,43 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const webpack = require('webpack'); // Se impporta en este caso para el hotModuleReplacement.
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/index.js')
-  },
+    app: path.resolve(__dirname, 'src/index.js')// De aquí toma el archivo javascript principal que se empaquetará.
+  },                                            // En este caso un solo entrypoint para una Single Page Aplication.
   mode: 'development', 
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),// El archivo ya procesado aparece en la carpeta dist
+    filename: 'bundle.js',                // Con el nombre de bundle.js
+    publicPath: 'http://localhost:3000/',
+    chunkFilename: 'js/[id].[chunkhash].js'
   },
   devServer: {
-    contentCase: path.resolve(__dirname, 'dist'),
-    hot: true,
-    open: true,
-    port: 3000
+    contentBase: path.resolve(__dirname, 'dist'), // Para que el contenido base los tome desde la carpeta de 'dist'.
+    hot: true, // Es para activar el hotModuleReplacement.
+    open: true, //Para que se abra automáticamente al ejecutar el comando.
+    port: 3000 // Es el puerto por donde correrá la aplicación.
   },
   module: {
     rules:[
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
+        test: /\.js$/, // Que tipo de archivos son los que va a transpilar babel.
+        use: 'babel-loader', // Es el loader que se va a usar.
+        exclude: /node_modules/, // Aquí se coloca lo que se va a ignorar
       },
       {
-        test:/\.css$/,
+        test:/\.css$/, // Es el tipo de archivos que va a soportar el loader.
         use: [
-          'style-loader',
-          'css-loader'
+          'style-loader', // Con este loader inyecta el css.
+          'css-loader' // Con este loader le da soporte a css.
         ]
       },
       {
-        test:/\.jpg|jpeg|png|svg|gif|woff|eot|ttf|mp4|webm$/,
+        test:/\.jpg|jpeg|png|svg|gif|woff|eot|ttf|mp4|webm$/, // Estos son  los tipos de archivos que soportará
         use: [
           {
             loader: 'file-loader', // Exporta los archivos y les da un enlace.
-            options:{
-              outputPath: 'assets/'
+            options:{ // Son opciones de configuración.
+              outputPath: 'assets/' //Enviaremos los archivos exportados a una carpeta llamada assets.
             }
           },
         ]
@@ -45,10 +47,10 @@ module.exports = {
     ]
   },
   plugins:[
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(), 
     new HtmlWebpackPlugin({
       title: 'Webpack 4',
-      template:path.resolve(__dir, 'dist/index.html') //dice donde va a estar ubicado el template.
+      template: path.resolve(__dirname, 'public/index.html') //le indica a webpack donde buscar el index.html.
     })
   ]
 } 
