a8a266635ceadd5c4644e4b89025ead602eb9e7b
koke <jorge.arancibia.jeal@gmail.com>
2019-09-29 04:02:53 -0300

    se agregaron los hash necesarios en produccion y hasta aqui todo bien con el codigo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -8,27 +10,34 @@ module.exports = {
   },                                            // En este caso un solo entrypoint para una Single Page Aplication.
   mode: 'production', 
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[hash].js',
+    publicPath: 'http://localhost:3001/',
+    chunkFilename: 'js/[id].[chunkhash].js'
   },
   module: {
     rules:[
       {
-        test:/\.css$/,
+        test: /\.js$/, // Que tipo de archivos son los que va a transpilar babel.
+        use: 'babel-loader', // Es el loader que se va a usar.
+        exclude: /node_modules/, // Aquí se coloca lo que se va a ignorar
+      },
+      {
+        test:/\.css$/, // Aquí van los tipos de archivos que va a soportar
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader // Esto extrae el css y lo exporta.
           },
-          'css-loader'
+          'css-loader' // Este es el loader que soporta css.
         ]
       },
       {
-        test:/\.jpg|jpeg|png|svg|gif|woff|eot|ttf|mp4|webm$/,
+        test:/\.jpg|jpeg|png|svg|gif|woff|eot|ttf|mp4|webm$/, // Tipos de archivos que se van a soportar.
         use: [
           {
-            loader: 'url-loader',
+            loader: 'url-loader', // loader que convierte archivos que no sobrepasan el limite a base64
             options:{
-              limit: 1000
+              limit: 1000 // Los que sobrepasan el límite se exportan.
             }
           },
         ]
@@ -36,12 +45,21 @@ module.exports = {
     ]
   },
   plugins:[
+    new MiniCssExtractPlugin({
+      filename:'css/[name].css',
+      chunkFilename: 'css/[id].css'
+    }),
     new HtmlWebpackPlugin({
       title: 'Webpack 4',
-      template: path.resolve(__dirname, 'public/index.html')
+      template: path.resolve(__dirname, 'public/index.html') // Toma el index.html e inserta el javascript generado en el output.
     }),
-    new MiniCssExtractPlugin({
-      filename:'css/[name].css'
+    new webpack.DllReferencePlugin({
+      manifest: require('./modules-manifest.json')
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, 'dist/js/*.dll.js'), // Qué archivos importar al html
+      outputPath: 'js',  //Dónde liberar los archivos, en qué carpeta.
+      publicPath: 'http://localhost:3001/js' // Desde dónde quiero leer ese archivo(Que lo lea de la carpeta 'js').}
     })
   ]
 }
--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -12,14 +12,14 @@ module.exports = {
   },
   mode: 'production', 
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[hash].dll.js',
     library: '[name]'
   },
   plugins:[
     new webpack.DllPlugin({
       name:'[name]',
-      path:path.join(__dirname, '[name]-manifest.json')
+      path: path.join(__dirname, '[name]-manifest.json')
     })
   ]
 }
